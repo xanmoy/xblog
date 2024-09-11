f24ad91a0be33b29b7ceeb5826fcf7b31326a20a
Tanmoy Ganguly <xanmoy.tsx@gmail.com>
2024-09-12 00:33:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Home from './Home'
+import Navbar from './Navbar'
+function App() {
+
+
+
+
+  return (
+    <div className='App'>
+      <Navbar/>
+      <div className="content">
+        <Home/>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '192.168.29.162', // Allows access from any IP address
+  },
+})
